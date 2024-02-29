2f5e51a708c23357eb112aace4975acafdbb8c74
faixanmalik <faixan.malik554@gmail.com>
2024-02-29 12:51:46 +0500

    update collection new

--- app/products/collections/page.tsx
+++ app/products/collections/page.tsx
@@ -7,13 +7,24 @@ import { Collection } from "@/types/collection";
 export const dynamic = "force-dynamic"
 
 export default async function CollectionsPage() {
-  const res = await fetch(apiUrl("/api/products/collections"), {
-    cache: "no-cache",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch");
+  try {
+    const res = await fetch(apiUrl("/api/products/collections"), {
+      cache: "no-cache",
+    });
+  
+    if (!res.ok) {
+      throw new Error("Failed to fetch");
+    }
+  
+    const collections: Collection[] = await res.json();
+    
+    // Process collections data
+  
+  } catch (error) {
+    console.error("An error occurred while fetching collections:", error);
+    // Handle the error gracefully, display a message to the user, or take other appropriate action
   }
-  const collections: Collection[] = await res.json();
+  
 
   return (
     <div className="bg-gray-100 min-h-screen md:px-8 py-8">
